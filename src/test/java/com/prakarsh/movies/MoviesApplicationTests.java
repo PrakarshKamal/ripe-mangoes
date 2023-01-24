c22f8964d74702ec6f6bbf743bf0ac09bd8fd93f
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-24 14:40:20 -0500

    test files

---
+++ src/test/java/com/prakarsh/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakarsh.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
