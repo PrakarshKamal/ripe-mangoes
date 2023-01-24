457fd46e9109b4376c46cee2867f5254da903614
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-24 14:42:52 -0500

    main file

---
+++ src/main/java/com/prakarsh/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.prakarsh.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
