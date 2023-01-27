4d71acc3fa9fefa5d8cfae23f4b275edd8f5a59c
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-26 20:54:52 -0500

    rendering all components + api request

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import api from './api/axiosConfig'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/home/Home'
+import './App.css'
+import Header from './components/header/Header'
+import Trailer from './components/trailer/Trailer'
 
 function App() {
+  const [movies, setMovies] = useState()
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/v1/movies')
+      console.log(response.data)
+      setMovies(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
