a280339d0ce79d17c7565b15bc2e0147a5115913
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-27 15:04:28 -0500

    add more routes + useStates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,16 @@ import api from './api/axiosConfig'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/home/Home'
-import './App.css'
 import Header from './components/header/Header'
 import Trailer from './components/trailer/Trailer'
+import Reviews from './components/reviews/Reviews'
+import './App.css'
+import NotFound from './components/notFound/NotFound'
 
 function App() {
   const [movies, setMovies] = useState()
+  const [movie, setMovie] = useState()
+  const [reviews, setReviews] = useState()
 
   const getMovies = async () => {
     try {
@@ -24,6 +28,20 @@ function App() {
     getMovies()
   }, [])
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`)
+
+      const singleMovie = response.data
+
+      setMovie(singleMovie)
+
+      setReviews(singleMovie.reviews)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
       <Header />
@@ -31,7 +49,19 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
         </Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   )
